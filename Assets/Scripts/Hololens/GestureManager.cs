6875324b90be7a1673e9efa36214a4a9f0de7e38
XueJia Wang <XueJia Wang>
2017-04-09 19:51:14 +0800

    feat: add Tap event
    1.if you focus a gameobject and tap,other objects  will hide,and the one
    you are selected will be closer to you
    2.tap again to cancel te select
    
    Signed-off-by: XueJia Wang <XueJia Wang>

---
+++ Assets/Scripts/Hololens/GestureManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class GestureManager : Singleton<GestureManager>
+{
+    /// <summary>
+    /// To rotation gameobject and select it
+    /// </summary>
+    public GestureRecognizer NavigationRecognizer
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// Currectly actived Recognizer
+    /// </summary>
+    public GestureRecognizer ActiveRecognizer
+    {
+        get;
+        private set;
+    }
+
+    private void OnEnable()
+    {
+        NavigationRecognizer = new GestureRecognizer();
+        NavigationRecognizer.SetRecognizableGestures(GestureSettings.Tap);
+
+        NavigationRecognizer.TappedEvent += NavigationRecognizer_TappedEvent;
+
+        //the default recognizer is to navigate
+        SwitchRecognizer(NavigationRecognizer);
+    }
+
+
+    /// <summary>
+    /// to switch recongnizer in cast there are not only one recognizer
+    /// </summary>
+    /// <param name="newRecognizer"> The recognizer to be started</param>
+    void SwitchRecognizer(GestureRecognizer newRecognizer)
+    {
+        if (newRecognizer == null)
+        {
+            return;
+        }
+        if (ActiveRecognizer != null)
+        {
+            if (ActiveRecognizer == newRecognizer)
+            {
+                return;
+            }
+            ActiveRecognizer.CancelGestures();
+            ActiveRecognizer.StopCapturingGestures();
+        }
+        newRecognizer.StartCapturingGestures();
+        ActiveRecognizer = newRecognizer;
+    }
+
+    void NavigationRecognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        GameObject _focusedObject = InteractManager.Instance.FocusGameObject;
+        if (_focusedObject != null)
+        {
+            _focusedObject.SendMessage("OnSelect");
+        }
+    }
+}
--- Assets/Scripts/Hololens/Interact.cs
+++ Assets/Scripts/Hololens/Interact.cs
@@ -8,10 +8,26 @@ using UnityEngine;
 public class Interact : MonoBehaviour
 {
     private Material[] defaultMaterials;
+    private Interact[] InteractibleObject;
+
+    private static Interact _selectedGameObject;
 
     private void Start()
     {
         defaultMaterials = GetComponent<Renderer>().materials;
+
+        //Find all brother
+        Transform _parent= transform.parent;
+        InteractibleObject = new Interact[_parent.childCount];
+        for (int index = 0; index != _parent.childCount; ++index)
+        {
+            InteractibleObject[index] = _parent.GetChild(index).GetComponent<Interact>();
+        }
+        if (InteractibleObject.Length != _parent.childCount)
+        {
+            Debug.LogError("INTERACTIBLEOBJECT_INIT_FAILED");
+        }
+
     }
 
     void GazeEnter()
@@ -27,6 +43,44 @@ public class Interact : MonoBehaviour
         for (int i = 0; i != defaultMaterials.Length; ++i)
         {
             defaultMaterials[i].SetFloat("_Highlight", 0f);
+        }
+    }
+
+    void OnSelect()
+    {
+        SwitchGameobject();
+    }
+
+    /// <summary>
+    /// if the currenyly select gameobject has been selected,then cancel the select mode
+    /// which the select object will be closer to player,and others gameobjects will be hided
+    /// </summary>
+    void SwitchGameobject()
+    {
+        bool _cancelSelected = _selectedGameObject == this;
+        for (int index = 0; index != InteractibleObject.Length; ++index)
+        {
+            Interact temp = InteractibleObject[index];
+            if (temp != this)
+            {
+                temp.gameObject.SetActive(_cancelSelected);
+            }
+            else
+            {
+                Vector3 direction = Camera.main.transform.forward;
+                float factor = _selectedGameObject == this ? 1.0f : -1.0f;
+                temp.transform.position +=direction*factor;
+            }
+        }
+
+        //Update SelectGameobject
+        if (_cancelSelected)
+        {
+            _selectedGameObject = null;
+        }
+        else
+        {
+            _selectedGameObject = this;
         }
     }
 }
